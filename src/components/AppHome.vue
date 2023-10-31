d0f85da98d1cf44099ead2da01d9e8df3e42a5c8
Herenyon <118363119+Herenyon@users.noreply.github.com>
2023-10-31 16:52:35 +0100

    Revert "Revert "create tre pagine con route""

---
+++ src/components/AppHome.vue
@@ -0,0 +1,12 @@
+<script>
+
+export default {
+    name: "AppHome"
+}
+</script>
+<template>
+    <section>
+        <div><router-link to="search">search</router-link></div>
+        <router-link to="home">home</router-link>
+    </section>
+</template>
---
+++ src/components/SearchPage.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    name: "SearchPage"
+}
+
+</script>
+
+<template>
+    <section>
+        <div>SearchPage</div>
+    </section>
+</template>
