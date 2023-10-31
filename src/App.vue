3135c9179180a4192695fea9b5ab76d75b1b9cea
Christian01881 <chrispamo01881@gmail.com>
2023-10-31 16:58:32 +0100

    fix branch

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import HomePage from './components/HomePage.vue';
+import SearchPage from './components/SearchPage.vue';
+export default {
+
+    components: {
+        HomePage,
+        SearchPage,
+    }
+};
+
+</script>
+
+<template>
+    <router-view></router-view>
+</template>
+
+<style scoped></style>
