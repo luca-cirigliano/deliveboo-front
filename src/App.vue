d4a668e683c2431668ed64d7d06695f7798afd87
Herenyon <rikitur17@gmail.com>
2023-11-03 16:13:15 +0100

    prima stesura header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <script>
 import HomePage from './views/HomePage.vue';
 import RestaurantShow from './views/RestaurantShow.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue'
 export default {
 
     components: {
         HomePage,
         RestaurantShow,
+        AppHeader,
+        AppFooter,
+
     }
 };
 
 </script>
 
 <template>
+    <AppHeader></AppHeader>
     <router-view></router-view>
+    <AppFooter></AppFooter>
 </template>
 
 <style scoped></style>
