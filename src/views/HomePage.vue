60d8b6ce97f72566375848992fcc2bcfc950f550
DavideAnile <davide.anile16@gmail.com>
2023-11-04 17:51:58 +0100

    Edit : Cart method Added

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -9,6 +9,7 @@ export default {
             restaurants: [],
             filters: [],
             noResults: false,
+            
         };
     },
 
@@ -25,6 +26,7 @@ export default {
     },  
 
     methods: {
+        
         getRestaurants() {
             axios.get(this.baseUrl + 'api/restaurants/').then(res => {
                 this.restaurants = res.data.results;
@@ -123,7 +125,21 @@ export default {
                 <RestaurantCard :restaurant="restaurant"></RestaurantCard>
             </div>
 
+           
+
+
+         
+
+            
+
+
+
         </div>
+
+        
+
+
+
 
 
     </section>
--- src/views/RestaurantShow.vue
+++ src/views/RestaurantShow.vue
@@ -9,33 +9,113 @@ export default {
         baseUrl : 'http://127.0.0.1:8000/',
         restaurant: {},
         restaurantSlug: '',
+        cart : [],
+        showMenu : false,
+        TotalPrice : 0,
 
         }
     },
 
     mounted() {
+        
         this.restaurantSlug = this.$route.params.slug;
         this.getRestaurant();
     },
 
     methods: {
+
+        showOffcanvasMenu(){
+            this.showMenu ? this.showMenu = false : this.showMenu = true;
+        },
+
         getRestaurant() {
             axios.get(this.baseUrl + 'api/restaurants/' + this.restaurantSlug).then(res => {
+               
                 this.restaurant = res.data.results;
-                console.log(this.restaurant);
+                
             });
         },
+
+        AddItemToCart(dish){
+
+            this.cart.push(dish);
+            console.log(this.cart)
+        },
+
+        CalculateTotalPrice(cart){
+
+            this.TotalPrice = 0
+
+            cart.forEach(element => {
+                
+                this.TotalPrice = this.TotalPrice + parseFloat(element.price) 
+
+
+
+            });
+
+            
+
+        }
     },
 }
 </script>
 
 <template>
-    <div>
-        <ul>
-            <li v-for="dish in restaurant.dishes" :key="dish.id">
-                {{ dish.name }} - {{ dish.description }} - Price: {{ dish.price }}
-            </li>
-        </ul>
-        <p>{{ restaurant.name }}</p>
+    <h1 class="text-center">{{ restaurant.name }}</h1>
+
+    <div>{{ cart.length }} piatti nel tuo carrello</div>
+
+    <div v-if="restaurant.dishes" class="d-flex gap-3">
+        
+        
+            <div v-for="dish in restaurant.dishes" :key="dish.id" class="list-unstyled">
+                <div>
+                    {{ dish.name }} 
+                </div>
+                <div>
+                    {{ dish.description }} 
+                </div>
+                <div>
+                    Price: {{ dish.price }}
+                </div>
+                <div><button @click="AddItemToCart(dish), CalculateTotalPrice(cart)">Add to cart</button></div>
+            </div>
+        
+
     </div>
+
+    
+            
+       
+    <div v-else>
+
+        <div>Nessun piatto</div>
+
+    </div>
+
+
+    <!-- prova offcanvas -->
+
+
+    <button class="btn btn-primary" @click.prevent="showOffcanvasMenu()" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling">Enable body scrolling</button>
+
+        <div class="offcanvas offcanvas-end" :class="showMenu ? 'show' : ''" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Offcanvas with body scrolling</h5>
+            <button type="button" class="btn-close" @click.prevent="showOffcanvasMenu()" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <div v-for="cartElement in cart">
+                <div>{{ cartElement.name }}</div>
+                <div>{{ cartElement.price }}</div>
+                
+            </div>
+
+            <div>Total price = {{TotalPrice }} </div>
+        </div>
+        </div>
+
+
+    <!-- /prova offancavas -->
 </template>
