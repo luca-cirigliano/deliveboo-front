bef190b60f8cd4176c9c6fb5bf5309403a16e89a
Herenyon <rikitur17@gmail.com>
2023-11-07 17:26:42 +0100

    abbellimento header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -15,6 +15,23 @@ export default {
                     <button class="btn btn-outline-success" type="submit">Search</button>
                 </form>
             </div>
+            <!-- <div class="dropdown">
+                <i class="fa-solid fa-circle-user restaurant-type"></i>
+
+                    
+            </div> -->
+            <div class="dropdown">
+                <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fa-solid fa-circle-user restaurant-type"></i>
+                </button>
+
+
+                <ul class="dropdown-menu dropdown-menu-end ms-dropdown">
+                    <li><a class="dropdown-item" href="http://127.0.0.1:8000/login">Login</a></li>
+                    <li><a class="dropdown-item" href="http://127.0.0.1:8000/register">Register</a></li>
+                </ul>
+            </div>
+
         </div>
     </div>
 </template>
